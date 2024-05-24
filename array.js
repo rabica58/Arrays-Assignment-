64591426292ede06bc714f424baffc72384f0640
rabica58 <rabicajennie58@gmail.com>
2024-05-24 06:27:34 -0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,37 @@
+// Question 1
+var fruits = ["apple", "banana", "mango", "orange"];
+console.log(fruits);
+// Question 2
+var numbers = [10, 20, 30, 40];
+console.log(numbers);
+// Question 3
+var thirdfruit = fruits[2];
+console.log(thirdfruit);
+// Question 4
+numbers[1] = 25;
+console.log(numbers);
+// Question 5
+var fruit = ["strawberry", "pineapple", "mango", "apple"];
+fruit.push("grape");
+console.log(fruit);
+// Question 6
+var lastFruit = fruit.pop();
+console.log(fruit);
+// Question 7
+var firstFruit = fruit.shift();
+console.log(fruit);
+// Question 8
+fruit.unshift("kiwi");
+console.log(fruit);
+// Question 9
+fruit.splice(1, 2);
+console.log(fruit);
+// Question 10
+fruit.splice(2, 0);
+console.log(fruit);
+// Question 11
+var citrusFruit = fruit.splice(0, 2);
+console.log(citrusFruit);
+// Question 12
+var lastTwoFruit = fruit.splice(-2);
+console.log(lastTwoFruit);
